031a4fe11ad6bab83e2f3ea7cfae616fda574d9d
peggy19925 <peggy19925@gmail.com>
2020-05-09 19:14:41 +0800

    修正部分錯誤

--- Source/Robot.h
+++ Source/Robot.h
@@ -52,89 +52,64 @@ public:
 	{
 		allObjs.push_back(&body);
 		body.init("../Assets/Robot/body.obj", "../Assets/Robot/gundam.mtl", 0);
-		ulefthand.position = glm::vec3(0, 2.9, 0);
 
 		allObjs.push_back(&ulefthand);
 		ulefthand.init("../Assets/Robot/ulefthand.obj", "../Assets/Robot/gundam.mtl", 1);
-		ulefthand.position = glm::vec3(3.7, 1, -0.5);
 		
 		allObjs.push_back(&lefthand);
 		dlefthand.init("../Assets/Robot/dlefthand.obj", "../Assets/Robot/gundam.mtl", 2);
-		ulefthand.position = glm::vec3(3.7, 1, -0.5);
-
+		
 		allObjs.push_back(&lefthand);
 		lefthand.init("../Assets/Robot/lefthand.obj", "../Assets/Robot/gundam.mtl", 3);
-		ulefthand.position = glm::vec3(0, -3, 0);
-
+		
 		allObjs.push_back(&lshouder);
 		lshouder.init("../Assets/Robot/lshouder.obj", "../Assets/Robot/gundam.mtl", 4);
-		ulefthand.position = glm::vec3(3.7, 1, -0.5);
-
+		
 		allObjs.push_back(&head);
 		head.init("../Assets/Robot/head.obj", "../Assets/Robot/gundam.mtl", 5);
-		ulefthand.position = glm::vec3(0, 3.9, -0.5);
-
+		
 		allObjs.push_back(&urighthand);
 		urighthand.init("../Assets/Robot/urighthand.obj", "../Assets/Robot/gundam.mtl", 6);
-		ulefthand.position = glm::vec3(-3.9, 1.7, -0.2);
-
+		
 		allObjs.push_back(&drighthand);
 		drighthand.init("../Assets/Robot/drighthand.obj", "../Assets/Robot/gundam.mtl", 7);
-		ulefthand.position = glm::vec3(0, -3, 0);
-
+		
 		allObjs.push_back(&righthand);
 		righthand.init("../Assets/Robot/righthand.obj", "../Assets/Robot/gundam.mtl", 8);
-		ulefthand.position = glm::vec3(0, -6, 0);
-
+		
 		allObjs.push_back(&rshouder);
 		rshouder.init("../Assets/Robot/rshouder.obj", "../Assets/Robot/gundam.mtl", 9);
-		ulefthand.position = glm::vec3(-3.9, 1.1, -0.2);
-
+		
 		allObjs.push_back(&dbody);
 		dbody.init("../Assets/Robot/dbody.obj", "../Assets/Robot/gundam.mtl", 10);
-		ulefthand.position = glm::vec3(0, 2, -4.5);
-
+		
 		allObjs.push_back(&back2);
 		back2.init("../Assets/Robot/back2.obj", "../Assets/Robot/gundam.mtl", 11);
-		ulefthand.position = glm::vec3(0, -5.3, 0);
-
+		
 		allObjs.push_back(&uleftleg);
 		uleftleg.init("../Assets/Robot/uleftleg.obj", "../Assets/Robot/gundam.mtl", 12);
-		ulefthand.position = glm::vec3(1.8, -4.5, 0);
-
+		
 		allObjs.push_back(&dleftleg);
 		dleftleg.init("../Assets/Robot/dleftleg.obj", "../Assets/Robot/gundam.mtl", 13);
-		ulefthand.position = glm::vec3(1.8, -4.5, 0);
-
-		// allObjs.push_back(&leftfoot);
-		// urightleg.init("../Assets/Robot/leftfoot.obj", "../Assets/Robot/gundam.mtl", 14);
-		// leftfoot.position = glm::vec3(0, -3, 0);
-		// 
-		// allObjs.push_back(&urightleg);
-		// urightleg.init("../Assets/Robot/urightleg.obj", "../Assets/Robot/gundam.mtl", 15);
-		// ulefthand.position = glm::vec3(0, -3, 0);
-		// 
-		// allObjs.push_back(&drightleg);
-		// drightleg.init("../Assets/Robot/drightleg.obj", "../Assets/Robot/gundam.mtl", 16);
-		// ulefthand.position = glm::vec3(0, -3, 0);
-		// 
-		// allObjs.push_back(&rightfoot);
-		// rightfoot.init("../Assets/Robot/rightfoot.obj", "../Assets/Robot/gundam.mtl", 17);
-		// ulefthand.position = glm::vec3(0, -3, 0);
-	}
-	void use() {
-		for (std::vector<MyObject*>::iterator iter = allObjs.begin();
-			iter != allObjs.end();
-			iter++
-			) {
-			(*iter)->use();
-		}
+		
+		allObjs.push_back(&leftfoot);
+		urightleg.init("../Assets/Robot/leftfoot.obj", "../Assets/Robot/gundam.mtl", 14);
+		
+		allObjs.push_back(&urightleg);
+		urightleg.init("../Assets/Robot/urightleg.obj", "../Assets/Robot/gundam.mtl", 15);
+		
+		allObjs.push_back(&drightleg);
+		drightleg.init("../Assets/Robot/drightleg.obj", "../Assets/Robot/gundam.mtl", 16);
+		
+		allObjs.push_back(&rightfoot);
+		rightfoot.init("../Assets/Robot/rightfoot.obj", "../Assets/Robot/gundam.mtl", 17);
 	}
 	void setMt(glm::mat4x4* modelMt, glm::mat4x4* viewMt = nullptr, glm::mat4x4* projectMt = nullptr) {
 		for (std::vector<MyObject*>::iterator iter = allObjs.begin();
 			iter != allObjs.end();
 			iter++
 			) {
+			(*iter)->use();
 			this->modelMt = *modelMt;
 			(*iter)->setViewProjectMt(viewMt, projectMt);
 		}
@@ -142,21 +117,59 @@ public:
 	void update() {
 		updatePos(0);
 		glm::mat4x4 modelMt = this->modelMt;
+		modelMt = glm::rotate(PI, glm::vec3(0, 0, 1));
 		stackMat.push_back(modelMt);
 		modelMt = body.setModelMt(&modelMt);
+		body.position = glm::vec3(0, 2.9, 0);
+		stackMat.push_back(modelMt);
+
+		/////¥ª¤â/////
+		// ¥ª¤WÁu
+		ulefthand.setRotateZ(10);
 		modelMt = ulefthand.setModelMt(&modelMt);
-		modelMt = lefthand.setModelMt(&modelMt);
+		ulefthand.position = glm::vec3(3.7, 1, -0.5);
+
+		// ¥ªªÓ
+		modelMt = lshouder.setModelMt(&modelMt);
+
+		// ¥ª¤UÁu
+		modelMt = dlefthand.setModelMt(&modelMt);
+		
+		modelMt = rshouder.setModelMt(&modelMt);
+		modelMt = stackMat.back();
+		stackMat.pop_back();
+		stackMat.push_back(modelMt);
+		modelMt = urighthand.setModelMt(&modelMt);
+		modelMt = drighthand.setModelMt(&modelMt);
+		modelMt = righthand.setModelMt(&modelMt);
+		
+		modelMt = stackMat.back();
+		stackMat.pop_back();
+		stackMat.push_back(modelMt);
+		
+		modelMt = uleftleg.setModelMt(&modelMt);
+		modelMt = dleftleg.setModelMt(&modelMt);
+		modelMt = leftfoot.setModelMt(&modelMt);
+		
+		modelMt = stackMat.back();
+		stackMat.pop_back();
+		stackMat.push_back(modelMt);
+		
+		modelMt = urightleg.setModelMt(&modelMt);
+		modelMt = drightleg.setModelMt(&modelMt);
+		modelMt = rightfoot.setModelMt(&modelMt);
+		stackMat.pop_back();
 		stackMat.pop_back();
 	}
 	void updatePos(int frame) {
 		switch (frame) {
 		case 0:
 			//¥ª¤â
-			ulefthand.rotation.y = -45;
+			ulefthand.setRotateY(-45);
 			//¥k¤â
 
 			//»L
-			uleftleg.rotation.y = 45;
+			ulefthand.setRotateY(45);
 			break;
 		//case 1:
 		//case 2:
